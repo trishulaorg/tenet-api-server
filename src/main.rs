8946d8064d093bb694a0bc09012091a846c84fa6
minamorl <minamorl@users.noreply.github.com>
2022-11-22 10:28:31 +0900

    initial impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use async_graphql::{http::GraphiQLSource, Context, Object, EmptyMutation, EmptySubscription, Schema};
+use async_graphql_poem::GraphQL;
+use poem::{get, handler, listener::TcpListener, web::Html, IntoResponse, Route, Server};
+
+
+pub struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn howdy<'a>(
+        &self,
+        ctx: &Context<'a>,
+        #[graphql(desc = "id of the human")] id: String
+    ) -> &'static str {
+        "partner"
+    }
+}
+
+#[handler]
+async fn graphiql() -> impl IntoResponse {
+    Html(
+        GraphiQLSource::build()
+            .endpoint("http://localhost:8000")
+            .finish(),
+    )
+}
+
+#[tokio::main]
+async fn main() {
+    // create the schema
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+
+    // start the http server
+    let app = Route::new().at("/", get(graphiql).post(GraphQL::new(schema)));
+
+    println!("GraphiQL IDE: http://localhost:8000");
+    Server::new(TcpListener::bind("127.0.0.1:8000"))
+        .run(app)
+        .await
+        .unwrap();
 }
