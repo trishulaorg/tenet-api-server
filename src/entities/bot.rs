d2853f16d408942c777476a3913fd15a7a4d8870
minamorl <minamorl@users.noreply.github.com>
2022-12-13 17:56:34 +0900

    fix shchema

--- src/entities/bot.rs
+++ src/entities/bot.rs
@@ -2,7 +2,7 @@
 
 use sea_orm::entity::prelude::*;
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, SimpleObject)]
 #[sea_orm(table_name = "Bot")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
