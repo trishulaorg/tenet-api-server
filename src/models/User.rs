f7eeecd1793a4b9e0ffa89530b6b22547f07fd9d
minamorl <minamorl@users.noreply.github.com>
2022-12-13 13:34:49 +0900

    wip

--- src/models/User.rs
+++ src/models/User.rs
@@ -4,6 +4,7 @@ use sea_orm::entity::prelude::*;
 use serde::{Serialize, Deserialize};
 use chrono::{Utc, Local, DateTime, Date};
 
+#[derive(SimpleObject)]
 #[derive(Serialize, Deserialize)]
 #[derive(Clone, Debug, PartialEq)]
 #[sea_orm(table_name = "user")]
